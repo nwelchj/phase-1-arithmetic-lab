d4934dc107da6f9f51fd15ee39d41ba000b71fd2
Nolan <bugygamer.mc@gmail.com>
2023-02-01 21:28:59 -0700

    Compl

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+// Write your code h
+ var num1 = 2
+ var num2 = 31
+let multiply = num2*num1
+
+let random 
+random = Math.floor(Math.random()*10)+1;
+
+var num3 = 8
+var num4 = 2
+var num5 = 20
+let mod = num3/num4
+let max
+max = Math.max(num1,num3,num4, num5);
